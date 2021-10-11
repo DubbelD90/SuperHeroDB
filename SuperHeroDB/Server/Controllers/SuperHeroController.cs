c039f3a5baad2106fc514052b26d086f15f8b257
dcloc <dcloc@DESKTOP-S4RJFLK>
2021-10-11 15:32:25 +0200

    Add project files.

---
+++ SuperHeroDB/Server/Controllers/SuperHeroController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroDB.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHeroDB.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        static List<Comic> comics = new List<Comic>
+        {
+            new Comic {Name = "marvel"},
+            new Comic {Name = "DC Universe"}
+        };
+
+        List<SuperHero> heroes = new List<SuperHero>
+        {
+            new SuperHero {FirstName = "Peter", LastName = "Parker", HeroName = "Spiderman", Comic = comics[0]},
+            new SuperHero {FirstName = "Bruce", LastName = "Wayne", HeroName = "Batman", Comic = comics[1]}
+        };
+
+        public async Task<IActionResult> GetSuperHeroes()
+        {
+            return Ok(heroes);
+        }
+    }
+}
